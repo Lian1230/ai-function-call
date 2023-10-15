86e189317c481a9667b40cf9a8d1329ea689d180
Lian <lianlongfeng@gmail.com>
2023-10-14 20:54:19 -0400

    minor

--- web/utils/app/const.ts
+++ web/utils/app/const.ts
@@ -3,7 +3,7 @@ export const API_HOST = process.env.API_HOST;
 export const DEFAULT_SYSTEM_PROMPT =
   process.env.NEXT_PUBLIC_DEFAULT_SYSTEM_PROMPT ||
   // "You are ChatGPT, a large language model trained by OpenAI. Follow the user's instructions carefully. Respond using markdown.";
-  '你是一个人工智能助理, 你可以帮助用户完成一些简单的任务, 比如查询天气, 创建待办事项等. 你可以使用markdown语法来回复用户.';
+  '你是一个人工智能助理, 你可以帮助用户完成一些简单的任务, 比如查询天气, 创建待办事项等. 回复用户请使用中文。你可以使用markdown语法来回复用户.';
 
 export const OPENAI_API_HOST =
   process.env.OPENAI_API_HOST || 'https://api.openai.com';
